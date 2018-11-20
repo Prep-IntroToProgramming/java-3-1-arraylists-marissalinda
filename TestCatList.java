4a1a28d47e0737dd070120ade8c43f9694552a36
Marissa Starks <19starksm@flintridgeprep.org>
2018-11-20 10:39:00 -0800

    project complete

--- TestCatList.java
+++ TestCatList.java
@@ -2,13 +2,24 @@ import java.util.ArrayList;
 public class TestCatList
 {
     public static void main(String[] args){
-      ArrayList<Cat> CatList = new ArrayList<Cat>();
-      for(int i=0; i<4; i++){
-          CatList.add(new Cat());
+        ArrayList<Cat> CatList = new ArrayList<Cat>();
+        for(int i=0; i<4; i++){
+            CatList.add(new Cat());
         }
-      CatList.get(0).setName("John");
-      CatList.get(1).setName("Mary");
-      CatList.get(2).setName("Belle");
-      CatList.get(3).setName("Dex");
+        CatList.get(0).setName("John");
+        CatList.get(1).setName("Mary");
+        CatList.get(2).setName("Mark");
+        CatList.get(3).setName("Dido");
+        for(Cat Cat: CatList){
+            Cat.setWeight(Math.random()*10+5);     
+        }
+        System.out.println(CatList.get(0).getName() + " weighs " + CatList.get(0).getWeight() + " pounds");
+        CatList.get(0).meow();
+        System.out.println(CatList.get(1).getName() + " weighs " + CatList.get(1).getWeight() + " pounds");
+        CatList.get(1).meow();    
+        System.out.println(CatList.get(2).getName() + " weighs " + CatList.get(2).getWeight() + " pounds");
+        CatList.get(2).meow();
+        System.out.println(CatList.get(3).getName() + " weighs " + CatList.get(3).getWeight() + " pounds");
+        CatList.get(3).meow();
     }
 }
