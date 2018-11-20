d8f98f70bf213576454d729d5f696455940b0950
Marissa Starks <19starksm@flintridgeprep.org>
2018-11-20 06:39:51 -0800

    completed step 0.5

--- Cat.java
+++ Cat.java
@@ -1,3 +1,21 @@
 
 public class Cat {
+    private String name;
+    private double weight;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(newName) {
+        name = newName;
+    }
+    
+    public double getWeight() {
+        return weight;
+    }
+    
+    public void setWeight(newWeight) {
+        weight = newWeight;
+    }
 }
+
